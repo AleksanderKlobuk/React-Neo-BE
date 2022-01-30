fe06a8b7069c7c39be10da33cd9e4b9304887465
AleksanderKlobuk <aleksander80k@gmail.com>
2022-01-30 17:34:52 +0100

    1th

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/api/register', (req, res)=>{
+    console.log(req.body)
+    res.json({status: 'okkkkk'})
+})
+app.get('/api/register', (req, res)=>{
+    res.send('User new form')
+})
+
+
+app.listen(1000, ()=>{
+    console.log('server started on 1000')
+})
+
+
+/*app.get("/api/data", (req,res)=>{
+    const customers = [
+        {id: 1, firstname: 'Alek', lastName: 'Klobuk'},
+        {id: 1, firstname: 'Kocz', lastName: 'Pocz'},
+        {id: 1, firstname: 'Ktil', lastName: 'Fluk'},
+    ];
+    res.json(customers);
+});
+*/
+
